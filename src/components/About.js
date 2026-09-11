46bc280cf122b69928cd3f0a40460bcae2efcf28
Karan3304 <163971189+Karan3304@users.noreply.github.com>
2026-09-11 23:12:47 +0530

    added a class based componenet UserClass to understand class based componenets

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,13 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = () => {
   return (
     <div>
       <h1>About</h1>
       <h2>this is OG react learning</h2>
+      {/* <User name={"Karan swami (function)"} location={"Indore"}/> */}
+      <UserClass name={"Karan swami (class)"} location={"America"}/>
     </div>
   );
 };
