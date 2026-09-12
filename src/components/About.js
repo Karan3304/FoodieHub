82222b5a5034942593b1005dc89c9e50e2c7bb7a
Karan3304 <163971189+Karan3304@users.noreply.github.com>
2026-09-12 20:59:54 +0530

    added class based component UserClass.js which have various lifecycle methods like componentdidmount,componentdidupdate,componentwillunmount

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,25 @@
 import User from "./User";
 import UserClass from "./UserClass";
+import React from "react";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-      <h2>this is OG react learning</h2>
-      {/* <User name={"Karan swami (function)"} location={"Indore"}/> */}
-      <UserClass name={"Karan swami (class)"} location={"America"}/>
-    </div>
-  );
-};
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    // console.log("Parent constructor");
+  }
+  componentDidMount() {
+    // console.log("parent compnenet did mount");
+  }
+  render() {
+    // console.log("Parent render");
+    return (
+      <div>
+        <h1>About class component</h1>
+        <h2>this is OG react learning</h2>
+        <UserClass name={"First"} location={"America"} />
+      </div>
+    );
+  }
+}
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,30 +5,42 @@ class UserClass extends React.Component {
     super(props);
     console.log(props);
     this.state = {
-      count: 0,
-      count2: 2,
+      userInfo: {
+        name: "Dummy name",
+        bio: "Dummy bio",
+        avatar_url: "dummy_url",
+      },
     };
+    // console.log(this.props.name + " child constructor");
   }
 
+  async componentDidMount() {
+    // console.log(this.props.name + " child compnenet did mount");
+    // API call
+    const data = await fetch("https://api.github.com/users/Karan3304");
+    const json = await data.json();
+    console.log(json);
+    this.setState({
+      userInfo: json,
+    });
+  }
+
+  componentDidUpdate() {
+    console.log("Component updated");
+  }
+
+  componentWillUnmount() {
+    console.log("component is unmounted");
+  }
   render() {
-    const { name, location } = this.props;
-    const { count, count2 } = this.state;
+    // const { name, location } = this.props;
+    // console.log(this.props.name + " child render");
+    const { name, bio, avatar_url } = this.state.userInfo;
     return (
       <div className="user-card">
-        <h1>Count1 : {count}</h1>
-        <h1>Count2 : {count2}</h1>
-        <button
-          onClick={() => {
-            this.setState({
-              count: this.state.count + 1,
-              count2: this.state.count2 + 1,
-            });
-          }}
-        >
-          Count Increase
-        </button>
+        <img src={avatar_url} />
         <h2>Name:{name}</h2>
-        <h3>Location:{location}</h3>
+        <h3>Location:{bio}</h3>
         <h4>Contact:@karans3304</h4>
       </div>
     );
