9c9f52c602f6f2969108530389eb085fa50c60ec
Karan3304 <163971189+Karan3304@users.noreply.github.com>
2026-09-18 23:24:26 +0530

    feat: add restaurant menu with dish categories and item listing
    - Add menu view accessible from each restaurant card
    - Display dish categories on opening a restaurant
    - Show list of items/dishes on selecting a category

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,10 +1,41 @@
+import { CDN_URL } from "../utils/constants";
 const ItemList = ({ items }) => {
   console.log(items);
   return (
     <div>
       <ul>
         {items.map((item) => (
-          <div key={item.card.info.id}></div>
+          <div
+            key={item.card.info.id}
+            className="p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between"
+          >
+            <div className="w-9/12">
+              <div className="py-2">
+                <span className="font-bold">{item.card.info.name}</span>
+                <span>
+                  - ₹
+                  {item.card.info.price
+                    ? item.card.info.price / 100
+                    : item.card.info.defaultPrice / 100}
+                </span>
+              </div>
+              <p className="text-xs">{item.card.info.description}</p>
+            </div>
+
+            <div className="w-3/12 p-4">
+              <div className="relative w-32 h-32 ml-auto flex items-center justify-center">
+                <button className="absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 px-4 py-1 bg-white shadow-lg rounded-lg text-green-600 font-bold border border-gray-200 z-10">
+                  Add+
+                </button>
+                {item.card.info.imageId && (
+                  <img
+                    className="w-32 h-32 rounded-lg shadow-md shadow-gray-400 object-cover"
+                    src={CDN_URL + item.card.info.imageId}
+                  />
+                )}
+              </div>
+            </div>
+          </div>
         ))}
       </ul>
     </div>
